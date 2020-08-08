961e0692082b6d9007a1992b54c2a1f3d8e995ab
Viktor <macedonian.wow@live.com>
2020-08-08 10:40:15 +0200

    Option to delte a Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -34,8 +34,9 @@ class RecipesController < ApplicationController
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
     @recipe.destroy
-    redirect_to root_path, notice: 'Successfully deleted recipe'
+    redirect_to recipes_path, notice: 'Successfully deleted recipe'
   end
 
   private
